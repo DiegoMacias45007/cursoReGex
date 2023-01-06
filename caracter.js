0522d5f38a0352be9cf89b8911992c90e228ccac
Diego Macias <djoromac@hotmail.com>
2023-01-06 01:21:27 -0500

    clases y delimitadores

---
+++ caracter.js
@@ -0,0 +1,23 @@
+/**
+ * Un caracter es cada pieza de informacion que conforma una palabra o numero
+ * Existen caracteres invicibles (El espacio)
+ * cada espacio se llena de un caracter, siendo al representacion grafica de un codigo askey
+ */
+1
+12
+123
+1234
+12345
+123456
+1234567
+12345678
+123456789
+12345678910
+'12345678910a'
+'hola'
+'palabra'
+
+/*Para encontrar un caracter utilizo '.', este denota cualquier caracter
+Puedo decir, encuentra '. ' y este me va a regresar todos los caracteres que tengan un espacio despues
+Si yo quisiera encontrar diez caracteres juntos: '..........'
+*/
